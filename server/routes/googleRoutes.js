9a2da3f9a82e0fb9533a48233bfab9ef3e08d329
anuskamhzn <anuskamhzn33@gmail.com>
2025-04-10 09:27:11 +0545

    ui fixed for task/project

--- server/routes/googleRoutes.js
+++ server/routes/googleRoutes.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const googleController = require('../controllers/googleController');
+// const express = require('express');
+// const googleController = require('../controllers/googleController');
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get('/auth', googleController.googleAuth);
-router.get('/callback', googleController.googleCallback);
-router.post('/event', googleController.createEvent);
-// Route to list events from Google Calendar
-router.get('/events', googleController.listEvents);
+// router.get('/auth', googleController.googleAuth);
+// router.get('/callback', googleController.googleCallback);
+// router.post('/event', googleController.createEvent);
+// // Route to list events from Google Calendar
+// router.get('/events', googleController.listEvents);
 
 
-module.exports = router;
+// module.exports = router;
